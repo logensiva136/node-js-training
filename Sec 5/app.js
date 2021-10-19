658bc5a2ffcbe63266adf8b56a92b7b8af6fb18c
logensiva136 <logensiva136@gmail.com>
2021-10-19 22:19:35 +0800

    mod at url path & installing body-parser

--- Sec 5/app.js
+++ Sec 5/app.js
@@ -1,16 +1,23 @@
-const http = require("http");
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next();
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST" ><input type="text" name="title" /><input type="submit" value="submit" /></form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Thanks ExpressJs</h1>");
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express JS</h1>');
 });
 
 app.listen(3000);
