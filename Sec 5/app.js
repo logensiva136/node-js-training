ca4ddb7ded1b6d68cb3dd88e52dfe00231aa640e
logensiva136 <logensiva136@gmail.com>
2021-10-19 22:42:20 +0800

    add router

--- Sec 5/app.js
+++ Sec 5/app.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { urlencoded } = require('body-parser');
 
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST" ><input type="text" name="title" /><input type="submit" value="submit" /></form>'
-  );
-});
-
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express JS</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
