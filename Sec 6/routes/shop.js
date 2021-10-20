20da135b4cee141923fa8a2b98b209473c5a6df1
logensiva136 <logensiva136@gmail.com>
2021-10-20 22:45:14 +0800

    adding templating & remove unused files

--- Sec 6/routes/shop.js
+++ Sec 6/routes/shop.js
@@ -5,9 +5,14 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {
+    prods: adminData.products,
+    pageTitle: 'Shop',
+    path: '/',
+  });
 });
 
 module.exports = router;
