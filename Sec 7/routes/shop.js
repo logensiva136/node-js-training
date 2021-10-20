22b43e126e42fbd1a89b9609a599bb624562e0fb
logensiva136 <logensiva136@gmail.com>
2021-10-20 23:02:03 +0800

    Starting Sec 7

---
+++ Sec 7/routes/shop.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+const adminData = require('./admin');
+
+router.get('/', (req, res, next) => {
+  res.render('shop', {
+    prods: adminData.products,
+    pageTitle: 'Shop',
+    path: '/',
+  });
+});
+
+module.exports = router;
