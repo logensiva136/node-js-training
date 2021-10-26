cfc49b6c3fd965421042c4d9fefb8311ba8c3ee5
logensiva136 <logensiva136@gmail.com>
2021-10-26 17:26:50 +0800

    Adding Cart ejs

--- Sec 9/controllers/shop.js
+++ Sec 9/controllers/shop.js
@@ -1,21 +1,21 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll((products) => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
-      path: '/products'
+      path: '/products',
     });
   });
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll((products) => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
-      path: '/'
+      path: '/',
     });
   });
 };
@@ -23,20 +23,37 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
-    pageTitle: 'Your Cart'
+    pageTitle: 'Your Cart',
   });
 };
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
-    pageTitle: 'Your Orders'
+    pageTitle: 'Your Orders',
   });
 };
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
-    pageTitle: 'Checkout'
+    pageTitle: 'Checkout',
   });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (result) => {
+    res.render('shop/product-detail', {
+      product: result,
+      pageTitle: result.title,
+      path: '/products',
+    });
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect('/');
 };
